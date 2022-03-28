e53d2f2929c1940f72668663f5aeb25a08d302b5
jeffreyscholz <scholz.jeffrey@gmail.com>
2022-03-28 18:08:24 +0700

    multi threading

--- README.md
+++ README.md
@@ -5,9 +5,11 @@ For example
 `deposit278591A(uint256)` has a function selector of `00000070`
 but `deposit(uint256)` has a function selector of `b6b55f25`
 
-The function selector is computed as the first 8 bytes of the keccak256 of the function signature. The function signature excludes the variable names. For example, `sendValue(uint256 amount)` is not correct but `sendValue(uin256)` is.
+The function selector is computed as the first 4 bytes of the keccak256 of the function signature. The function signature excludes the variable names. For example, `sendValue(uint256 amount)` is not correct but `sendValue(uin256)` is.
 
-The gas cost of a function name is 4 times the number of zero bytes, and 12 times the number of nonzero bytes. So in the worst case, the gas cost is 96 gas, and in the best case 40 gas. An all zero function selector won't compile because that conflicts with the fallback function. Thus, `mint_22F5A30(uint256)` is more efficient than `mint(uint256)`
+The gas cost of a function name is 4 times the number of zero bytes, and 16 times the number of nonzero bytes. So in the worst case, the gas cost is 64 gas (4 non-zero bytes), and in the best case 28 gas (3 zero and 1 non-zero). An all zero function selector won't compile because that conflicts with the fallback function. Thus, `mint_22F5A30(uint256)` is more efficient than `mint(uint256)`
+
+If the zeros are leading zeros, this will make the contract smaller and save gas on deployment.
 
 ## Install Rust
 [https://www.rust-lang.org/tools/install](https://www.rust-lang.org/tools/install)
@@ -16,8 +18,14 @@ The gas cost of a function name is 4 times the number of zero bytes, and 12 time
 `cargo build --release`
 
 ## Run
-`./target/release/eth-zero-finder "myFunctionName?(uint,address,...)"`
+`./target/release/eth-zero-finder "myFunctionName?(uint,address,...)"<num threads> (true|false)`
 
 The question mark will be substituted with a hex number found by searching. Your function should follow the format for solidity function selectors. Eg: No arguments: "myFunction()". One address argument: "myFunction(address)". Two arguments: "anotherFunction(uint256,address)" etc.
 
-The search usually completes in less than 10 minutes on a macbook pro.
+Num threads is the number of threads to use. The final true false is if you want to force the zeros to be leading zeros. Leading zeros lead to a lower gas cost on deployment. 
+
+The search usually finds a solution in less than 30 seconds on a macbook pro with 8 threads.
+
+Example run:
+
+`./target/release/eth-zero-finder "withdraw_?()" 8 true`
--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,77 @@ use self::crypto::digest::Digest;
 use self::crypto::sha3::Sha3;
 use std::env;
 use std::process;
+use std::thread;
 
-fn main() {
-    let mut best = 0;
-    for i in 1..100000000 {
-        let args: Vec<String> = env::args().collect();
-        if args.len() != 2 {
-            println!("Wrong number of arguments");
-            process::exit(1);
+fn num_zeros(result: &String, leading: &bool) -> i32 {
+    let mut counter = 0;
+    for i in (0..8).step_by(2) {
+        if &result[i..(i+2)] == "00" {
+            counter += 1;
         }
-        let function_name = &args[1]; 
+        else {
+            if *leading {
+                break;
+            }
+        }
+    }
+    return counter;
+}
+
+fn search(start: usize, step: &usize, function_name: &str, leading: &bool) {
+    let mut best = 0;
+    for i in (start..1000000000).step_by(*step) {
         let hex_num = format!("{:X}", i);
         let candidate = function_name.replace("?", &hex_num);
         let mut hasher = Sha3::keccak256();
 
         hasher.input_str(&candidate);
         let hash_result = hasher.result_str();
-        let num_zeros = hash_result[..8].matches("0").count();
-        if num_zeros > best {
-            best = num_zeros;
-            println!("{} {} {}", candidate, &hash_result[..8], &num_zeros);
+        let num_zero_bytes = num_zeros(&hash_result, &leading);
+        if num_zero_bytes == 4 {
+            continue;
+        }
+        if num_zero_bytes > best {
+            best = num_zero_bytes;
+            println!("{} {} {}", candidate, &hash_result[..8], &num_zero_bytes);
         }
 
-        // 8 zeros will collide with the fallback function
-        if num_zeros > 6 {
-            break;
+        if num_zero_bytes == 3 {
+            return;
         }
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 4 {
+        println!("Wrong number of arguments");
+        process::exit(1);
+    }
+    let funn =  &args[1];
+    let nt = match args[2].parse::<usize>() {
+        Ok(i) => i,
+        Err(_e) => 1,
+    };
+    let num_threads = nt.to_owned();
+    let leading_arg = &args[3];
+
+    if !(leading_arg == "true" || leading_arg == "false") {
+        println!("must specify if zero bytes are required to be leading bytes (true|false)");
+        process::exit(1);
+    }
+    let leading = if leading_arg == "true" { true } else { false };
+
+    let mut thread_handles: Vec<thread::JoinHandle<()>> = Vec::new();
+    for i in 0..num_threads {
+        let function_name = funn.to_owned();
+        let handle = thread::spawn(move || {
+            search(i, &num_threads, &function_name, &leading);
+        });
+        thread_handles.push(handle);
+    }
+
+    for handle in thread_handles {
+        handle.join().unwrap()
     }
 }
